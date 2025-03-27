8bc9c11b562ded4135cf75bbe701691ba8427e06
BjoernTroldahl <bjoerntrold@hotmail.com>
2025-03-27 23:17:20 +0100

    Can now drag the Water vessel

---
+++ Assets/Game 1- The Field/Scripts/3.WaterCrops/DragWaterVessel.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DragWaterVessel : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main Camera not found!");
+        }
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePosition.x + offset.x, mousePosition.y + offset.y, transform.position.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        offset = transform.position - mousePosition;
+        isDragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+}
