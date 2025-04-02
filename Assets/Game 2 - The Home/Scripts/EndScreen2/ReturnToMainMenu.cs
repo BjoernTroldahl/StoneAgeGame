127a0eab1ce10f5cd068c324f5555b2c8f7ea282
BjoernTroldahl <bjoerntrold@hotmail.com>
2025-04-02 21:19:23 +0200

    Started fixing bugs, corrections from Miro board

---
+++ Assets/Game 2 - The Home/Scripts/EndScreen2/ReturnToMainMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
